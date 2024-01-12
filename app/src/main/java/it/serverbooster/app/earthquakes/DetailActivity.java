d1a67d94ef11a9c0dd09a67ee7b61b8357dbfdab
Server-Booster <dicrescemanuel@gmail.com>
2024-01-12 17:12:38 +0100

    Added a share button into Activity Detail

--- app/src/main/java/it/serverbooster/app/earthquakes/DetailActivity.java
+++ app/src/main/java/it/serverbooster/app/earthquakes/DetailActivity.java
@@ -1,5 +1,6 @@
 package it.serverbooster.app.earthquakes;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,7 +23,17 @@ public class DetailActivity extends AppCompatActivity {
         Earthquake earthquake = (Earthquake) getIntent().getSerializableExtra(EXTRA_EARTHQUAKE);
         if(earthquake != null) {
             binding.setEarthquake(earthquake);
+            binding.shareButton.setOnClickListener(view -> initializeShareIntent(earthquake));
         }
-
     }
+
+    public void initializeShareIntent(Earthquake earthquake){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/html");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, earthquake.getUrl());
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, earthquake.getTitle());
+        shareIntent.putExtra(Intent.EXTRA_TITLE, earthquake.getTitle());
+        startActivity(Intent.createChooser(shareIntent, null));
+    }
+
 }
